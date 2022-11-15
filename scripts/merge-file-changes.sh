fe266e1f8d05667e8e01e2d8bf0fb5b17f44ddc3
Stefan Smith <stefan.smith@gousto.co.uk>
2022-11-15 22:14:25 +0000

    Don't fail hotspot analysis when a repository has no changes in the specified time period

--- scripts/merge-file-changes.sh
+++ scripts/merge-file-changes.sh
@@ -13,5 +13,6 @@ fileChangesLogFilePaths="$(echo "${fileChangesLogFilePathsString}" | tr ' ' '\n'
 if [ "$(echo "${fileChangesLogFilePaths}" | wc -l)" -eq 1 ]; then
   cat "${fileChangesLogFilePaths}"
 else
-  echo "${fileChangesLogFilePaths}" | xargs -I {}  -S 2048 "${scriptDirectoryPath}/prefix-file-changes-with-repo.sh" "{}" "${analysesDirectoryPath}" "${fileChangesLogFileRelativePath}"
+  # Last pipe-delimited statement collapses multiple empty lines together, since maat-scripts does not tolerate them
+  echo "${fileChangesLogFilePaths}" | xargs -I {}  -S 2048 "${scriptDirectoryPath}/prefix-file-changes-with-repo.sh" "{}" "${analysesDirectoryPath}" "${fileChangesLogFileRelativePath}" | sed -e '/^$/N' -e '/^\n$/D'
 fi
