107a92f7269da35c6a3734da6db4f9ce9993dd7e
Stefan Smith <stefan.smith@gousto.co.uk>
2022-11-17 21:22:08 +0000

    Avoid exceeding sed parameter length by concatenating data and html template

--- scripts/generate-hotspot-enclosure-diagram.sh
+++ scripts/generate-hotspot-enclosure-diagram.sh
@@ -9,4 +9,6 @@ scriptDirectoryPath=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null 
 
 enclosureDiagramData="$(python "${scriptDirectoryPath}/../maat-scripts/transform/csv_as_enclosure_json.py" --structure "${linesOfCodeReportFilePath}" --weights "${changeFrequencyReportFilePath}")"
 
-sed "s/\[\[replace-with-json-data\]\]/${enclosureDiagramData}/" "${scriptDirectoryPath}/../enclosure-diagram/enclosure-diagram.html"
+cat "${scriptDirectoryPath}/../enclosure-diagram/enclosure-diagram-header.html"
+echo "<script>var root=${enclosureDiagramData};</script>"
+cat "${scriptDirectoryPath}/../enclosure-diagram/enclosure-diagram-footer.html"
