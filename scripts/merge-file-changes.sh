c645bb4183a56d180b83c90336f853328e4b2ee5
Stefan Smith <stefan.smith@gousto.co.uk>
2022-11-29 17:06:19 +0000

    Bug fix: knowledge maps fail to be generated when some repos have no commits
    Caused by extra empty newline at start of file changes report. Not tolerated by code-maat.

--- scripts/merge-file-changes.sh
+++ scripts/merge-file-changes.sh
@@ -13,6 +13,6 @@ fileChangesLogFilePaths="$(echo "${fileChangesLogFilePathsString}" | tr ' ' '\n'
 if [ "$(echo "${fileChangesLogFilePaths}" | wc -l)" -eq 1 ]; then
   cat "${fileChangesLogFilePaths}"
 else
-  # Last pipe-delimited statement collapses multiple empty lines together, since maat-scripts does not tolerate them
-  echo "${fileChangesLogFilePaths}" | xargs -I {}  -S 2048 "${scriptDirectoryPath}/prefix-file-changes-with-repo.sh" "{}" "${analysesDirectoryPath}" "${fileChangesLogFileRelativePath}" | sed -e '/^$/N' -e '/^\n$/D'
+  # Use sed to collapse multiple empty lines together and then to remove any newlines from the start of the file. maat-scripts does not tolerate new lines in unexpected places
+  echo "${fileChangesLogFilePaths}" | xargs -I {}  -S 2048 "${scriptDirectoryPath}/prefix-file-changes-with-repo.sh" "{}" "${analysesDirectoryPath}" "${fileChangesLogFileRelativePath}" | sed -e '/^$/N' -e '/^\n$/D' | sed '/./,$!d'
 fi
