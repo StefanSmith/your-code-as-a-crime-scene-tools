b6a23fb3e7ea653d92e562456504a2ef06f33722
Stefan Smith <stefan.smith@gousto.co.uk>
2022-11-30 13:26:47 +0000

    Generify enclosure diagram generation script

--- scripts/generate-hotspot-enclosure-diagram.sh
+++ scripts/generate-hotspot-enclosure-diagram.sh
@@ -3,12 +3,12 @@
 set -o pipefail -o nounset -o errexit
 
 linesOfCodeReportFilePath="${1}"
-changeFrequencyReportFilePath="${2}"
+entityWeightsFilePath="${2}"
 
 scriptDirectoryPath=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-enclosureDiagramData="$(python "${scriptDirectoryPath}/../maat-scripts/transform/csv_as_enclosure_json.py" --structure "${linesOfCodeReportFilePath}" --weights "${changeFrequencyReportFilePath}")"
+enclosureDiagramData="$(python "${scriptDirectoryPath}/../maat-scripts/transform/csv_as_enclosure_json.py" --structure "${linesOfCodeReportFilePath}" --weights "${entityWeightsFilePath}")"
 
 cat "${scriptDirectoryPath}/../enclosure-diagram/enclosure-diagram-header.html"
 echo "<script>var root=${enclosureDiagramData};</script>"
-cat "${scriptDirectoryPath}/../enclosure-diagram/hotspot-enclosure-diagram-footer.html"
+cat "${scriptDirectoryPath}/../enclosure-diagram/heatmap-enclosure-diagram-footer.html"
